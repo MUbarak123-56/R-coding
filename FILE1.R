3349e97f6c333288dc9ab54ab0758ffc5392e676
TheDataMan <41446936+MUbarak123-56@users.noreply.github.com>
2020-04-26 12:14:46 -0500

    New commit

--- FILE1.R
+++ FILE1.R
@@ -1,1 +1,18 @@
 print("Hello World")
+mean(1:5)
+## help function can be used to figure out how a certain function works
+help("mean")
+??plotting
+## apropos can be used to see specific use cases of functions or 
+## specific functions that contains certain numbers or alphabets
+apropos("vector")
+apropos("z$")
+apropos("[4-9]")
+example(plot) ## gives an example of a function
+demo()         #list all demonstrations 
+demo(Japanese)
+browseVignettes() ## for browsing for packages online
+vignette("Sweave", package = "utils") ## for browsing for a specific package online
+RSiteSearch("{Bayesian regression}") ## For searching for specific R-related functions online
+sd(1:100)
+demo(plotmath)
